6bcc70eb93370505bbfb8b073db9b4ed6882c31f
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-06 20:30:42 -0300

    médoto de classe pegando os últimos valores pendentes

--- source/postgres/postgresql.py
+++ source/postgres/postgresql.py
@@ -9,3 +9,18 @@ class Postsql:
             host = scts.host
         )
         self.cursor_sql = self.conn.cursor()
+
+    def last_values(self):
+        """
+        Método de classe para retornar os 100 valores mais antigos pendentes tratativas.
+
+        id_package, table_0, substatus_package, status_package, date. precisam ser definidas no banco de dados.
+        """
+        self.cursor_sql.execute("""
+            SELECT id_package
+            FROM table_0
+            WHERE substatus_package IS NULL
+            OR status_package IN ('station', 'on_route', 'unknown')
+            ORDER BY date ASC
+            LIMIT 100;
+                                """)
