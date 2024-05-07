da3ff1e28570faf58d2cf6a9b77ac9a2f60cf59f
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-07 00:14:13 -0300

    novo método de classe

--- source/package/package.py
+++ source/package/package.py
@@ -5,6 +5,7 @@ class Package:
     def get_json(self, browser, url, id_json):
         
         import re
+        import json
 
         browser.driver.get(f'{url}{id_json}')
         page_content = browser.driver.page_source
@@ -18,6 +19,11 @@ class Package:
                 #export json files to data/json_files
                 with open(f'data/json_files/{id_json}.json', 'w', encoding='utf-8') as json_file:
                     json_file.write(json_data)
+            return json.loads(json_data)
         else:
             print(f'Error, page content not found.')
             raise FileNotFoundError
+    
+    def get_info(self, get_json):
+        if get_json:
+            ...
