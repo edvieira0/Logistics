cc1ecd7d073c24d853f5190c7364d73f98825b05
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-07 17:18:49 -0300

    instânciando classes para testes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from source.browser.browser import Browser
 from login import login
 from scts import user_logistic, password_logistic
+from source.package import package
 
-browser_instance = Browser('chrome')
+# browser_instance = Browser('chrome')
 
-login(browser_instance, user_logistic, password_logistic)
+# login(browser_instance, user_logistic, password_logistic)
+
+instance_teste = package.Package(Browser('chrome'))
+
+instance_teste.extract_infos('https://www.youtube.com/')
