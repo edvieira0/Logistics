9bac6f6f299e14a3591a0a33636106e38dee9cd1
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-07 17:18:29 -0300

    correção na lógica de extração de json files

--- source/package/package.py
+++ source/package/package.py
@@ -1,19 +1,18 @@
 class Package:
-    def __init__(self, package_id):
-        from scts import management_url
-        self.package_id = package_id
+    def __init__(self, browser):
+        self.browser = browser
+        # self.package_id = package_id
         self.export_data = True
-        self.url = management_url
 
-    def get_json(self, browser):
+    def get_json(self):
         """
         Função com o objetivo de varrer a página em busca de um json contendo informações.
         """
         import re
         import json
 
-        browser.driver.get(f'{self.url}{self.package_id}')
-        page_content = browser.driver.page_source
+        page_content = self.browser.driver.page_source
+        json_data = None
 
         if page_content:
             match = re.search(r'window\.__PRELOADED_STATE__\s*=\s*({.*?});', page_content, re.DOTALL)
@@ -26,16 +25,18 @@ class Package:
                     with open(f'data/json_files/{self.package_id}.json', 'w', encoding='utf-8') as json_file:
                         json_file.write(json_data)
 
+        if json_data is not None:
             return json.loads(json_data)
         else:
-            print(f'Error, page content not found.')
-            raise FileNotFoundError
+            raise FileNotFoundError('JSON not Found.')
+
+    def extract_infos(self, url):
+        self.browser.driver.get(url)
+        json_files = self.get_json()
+        print(json_files)
 
     def change_status(self):
         ...
 
-    def get_info(self):
-        ...
-    
     def update_database(self):
         ...
