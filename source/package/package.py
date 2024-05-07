d363fdddd9c040fc73de218b518adebdccfeb7d0
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-06 21:14:31 -0300

    correção de lógica e adição de exceções

--- source/package/package.py
+++ source/package/package.py
@@ -2,11 +2,12 @@ class Package:
     def __init__(self):
         pass
 
-    def get_json(self, browser, id_json):
+    def get_json(self, browser, url, id_json):
+        
         import re
 
-        browser.get(self.management_url)
-        page_content = browser.driver.page_source        
+        browser.driver.get(f'{url}{id_json}')
+        page_content = browser.driver.page_source
 
         if page_content:
             match = re.search(r'window\.__PRELOADED_STATE__\s*=\s*({.*?});', page_content, re.DOTALL)
@@ -17,3 +18,6 @@ class Package:
                 #export json files to data/json_files
                 with open(f'data/json_files/{id_json}.json', 'w', encoding='utf-8') as json_file:
                     json_file.write(json_data)
+        else:
+            print(f'Error, page content not found.')
+            raise FileNotFoundError
