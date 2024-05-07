90eebf8ecdefdb96a143360f9074c4595a3a8876
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-07 00:14:00 -0300

    comandos sql e servidor

---
+++ data/scts.py
@@ -0,0 +1,7 @@
+dbname = "postgres"
+user = "postgres"
+password = "7890"
+host = "localhost"
+
+default_url = "https://xtools.adminml.com/tools/home/"
+management_url = 'https://envios.adminml.com/logistics/package-management/package/'
