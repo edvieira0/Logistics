39cd6d66d134368ccd057a703dfdcc280c2b3ca1
Eduardo Vieira <eduardo.vieira.678@gmail.com>
2024-05-07 18:30:36 -0300

    instancias novas

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from source.browser.browser import Browser
-from login import login
 from scts import user_logistic, password_logistic
 from source.package import package
+from scts import management_url
 
-# browser_instance = Browser('chrome')
+packages_ids = [
+    43332964864,
+    43338888166,
+    43339921850,
+    43333804110,
+    43340133684,
+    43335804296,
+    43346570094,
+    43338049059,
+    43345158175,
+    43345083099,
+    43346123461,
+    43348423224,
+    43351657012,
+    43335559702
+]
 
-# login(browser_instance, user_logistic, password_logistic)
 
 instance_teste = package.Package(Browser('chrome'))
-
-instance_teste.extract_infos('https://www.youtube.com/')
+instance_teste.login(user_logistic, password_logistic)
+for i in packages_ids:
+    instance_teste.extract_infos(f'{management_url}{i}')
